91e003f3e81a288cd6aafb45115c2b619fae497f
Luriss <luowenbiao_92@163.com>
2018-05-11 11:12:17 +0800

    Initial Commit

---
+++ SpeedScale/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SpeedScale
+//
+//  Created by 1 on 2018/5/11.
+//  Copyright © 2018年 luris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SpeedScale/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SpeedScale
+//
+//  Created by 1 on 2018/5/11.
+//  Copyright © 2018年 luris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
